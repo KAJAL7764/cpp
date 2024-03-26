2887ffea21bb6292b505dc5bbcbb50abf9c69dad
kajal kumari <128963957+KAJAL7764@users.noreply.github.com>
2024-03-26 23:13:48 +0530

    Update binarytdecimal.cpp

--- binarytdecimal.cpp
+++ binarytdecimal.cpp
@@ -1,18 +1,22 @@
+//binary to decimal in cpp
+
 #include<iostream>
 using namespace std;
 
 int main(){
-    int BinNum;
-    int ans = 0, rem, mul = 1;
+    int BinNum;  // To store the binary number entered by the user
+    int ans = 0, rem, mul = 1;  // Variables to store the decimal equivalent
 
+       // Asking the user to enter the binary number
 cout <<"Enter the binary number : "<<endl;
 cin>>BinNum;
     while (BinNum > 0)
     {
-        rem= BinNum % 10;
-        BinNum =BinNum / 10;
-        ans = rem * mul + ans;
-        mul = mul * 2;
+        
+        rem= BinNum % 10;  // Extracting the last digit of the binary number
+        BinNum =BinNum / 10;  // Updating the binary number by removing the last digit
+        ans = rem * mul + ans;   // Calculating the decimal equivalent
+        mul = mul * 2;  // Multiplying the multiplier by 2 for the next digit
     }
     cout << ans <<endl;
     return 0;
